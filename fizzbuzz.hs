c50cf9338592c2472294172f39e40b48950a07bf
Stefan Scheller <stefan.scheller@endress.com>
2023-11-29 12:16:40 +0100

    added another fizzbuzz variant

--- fizzbuzz.hs
+++ fizzbuzz.hs
@@ -8,3 +8,10 @@ fizzbuzz limit = [ mapx x | x <- [1..limit]] where
         where
             divisibleBy5 x = x `mod` 5 == 0
             divisibleBy3 x = x `mod` 3 == 0
+
+fizzbuzz' :: (Integral a, Show a) => a -> [String]
+fizzbuzz' limit = [ mapx x | x <- [1..limit]] where
+    mapx x = 
+        let by5 = x `mod` 5 == 0
+            by3 = x `mod` 3 == 0
+        in (if by3 then "fizz" else "") ++ (if by5 then "buzz" else "") ++ (if not(by5 || by3) then show x else "")
